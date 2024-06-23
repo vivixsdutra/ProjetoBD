6ab9fba679a7c903f4463c3af4dd833cb9454f5d
Vitória de Moraes Dutra <123991484+vivixsdutra@users.noreply.github.com>
2024-06-23 19:20:10 -0300

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/biblioteca_gaia";
     private static final String USER = "root";
-    private static final String PASSWORD = "sua_senha";
+    private static final String PASSWORD = "BANCOdados123";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
